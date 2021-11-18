09b559fbf6830517d58d9172300ae7507ec43613
xshady <fastik036@gmail.com>
2021-11-19 00:29:16 +0300

    server: main class instance

--- server/src/xsync-entity/class.ts
+++ server/src/xsync-entity/class.ts
@@ -1,7 +1,21 @@
+import { IdProvider } from "../id-provider"
 import { WSServer } from "../ws-server"
 
 export class XSyncEntity {
+  private static _instance: XSyncEntity | null = null
+
+  public static get instance (): XSyncEntity {
+    const { _instance } = this
+
+    if (!_instance) {
+      throw new Error("XSyncEntity has not been initialized yet")
+    }
+
+    return _instance
+  }
+
   private readonly wss: WSServer
+  private readonly idProvider = new IdProvider()
 
   constructor (
     public readonly websocketPort = 7700,
@@ -12,5 +26,11 @@ export class XSyncEntity {
         events: {},
       },
     )
+
+    if (XSyncEntity._instance) {
+      throw new Error("XSyncEntity already initialized")
+    }
+
+    XSyncEntity._instance = this
   }
 }

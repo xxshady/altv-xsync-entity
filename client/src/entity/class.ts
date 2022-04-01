6015fea37ba3c04e098a687f18078a14ca1de826
xshady <fastik036@gmail.com>
2022-04-01 21:12:18 +0300

    add client Entity.getByID, improve entity data

--- client/src/entity/class.ts
+++ client/src/entity/class.ts
@@ -1,9 +1,16 @@
 import type * as alt from "alt-client"
 import type { EntityData } from "altv-xsync-entity-shared"
 import { InternalEntity } from "../internal-entity"
+import { InternalEntityPool } from "../internal-entity-pool"
 
 export abstract class Entity<T extends EntityData = EntityData> {
   private readonly internalInstance: InternalEntity
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public static getByID<T extends new (...args: any) => Entity> (this: T, id: number): InstanceType<T> | null {
+    const entity = InternalEntityPool.entities[id]?.publicInstance as InstanceType<T>
+    return (entity instanceof this) ? entity : null
+  }
 
   constructor (
     public readonly id: number,
--- shared/src/types/entity.ts
+++ shared/src/types/entity.ts
@@ -1,1 +1,3 @@
-export type EntityData = Record<string, unknown>
+// any because otherwise typescript will complain about index signature
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type EntityData = Record<any, any>

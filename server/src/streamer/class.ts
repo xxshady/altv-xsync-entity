e0fe7729784d8cce8fa33046811d05ac01d12fc5
xshady <fastik036@gmail.com>
2022-05-04 23:56:24 +0300

    server: disable debug logs

--- server/src/streamer/class.ts
+++ server/src/streamer/class.ts
@@ -58,14 +58,14 @@ export class Streamer {
         removedPlayerIds,
       } = this.currentPlayersUpdate
 
-      if (Object.keys(playersInEntities).length > 0) {
-        this.log.log("[streamIn]")
-        this.log.moreInfo(playersInEntities)
-      }
-      if (Object.keys(playersOutEntities).length > 0) {
-        this.log.log("[streamOut]")
-        this.log.moreInfo(playersOutEntities)
-      }
+      // if (Object.keys(playersInEntities).length > 0) {
+      //   this.log.log("[streamIn]")
+      //   this.log.moreInfo(playersInEntities)
+      // }
+      // if (Object.keys(playersOutEntities).length > 0) {
+      //   this.log.log("[streamOut]")
+      //   this.log.moreInfo(playersOutEntities)
+      // }
 
       // TODO: refactor this shit
 

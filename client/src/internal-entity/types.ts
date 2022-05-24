1d1e80693aeb98887347b190552b7f92b00d8fd5
xshady <fastik036@gmail.com>
2022-05-24 17:45:33 +0300

    client: fix entity events types

--- client/src/internal-entity/types.ts
+++ client/src/internal-entity/types.ts
@@ -1,12 +1,14 @@
 import type * as alt from "alt-client"
 import type { Entity } from "../entity"
 
+type EntitySyncedMeta<T extends Entity> = Readonly<T["syncedMeta"]>
+
 export interface IEntityEventHandlers<T extends Entity = Entity> {
   streamIn: (entity: T) => void
   streamOut: (entity: T) => void
   posChange?: (entity: T, pos: alt.IVector3) => void
-  syncedMetaChange?: (entity: T, syncedMeta: Partial<Entity["syncedMeta"]>) => void
-  netOwnerChange?: (entity: T, isLocalPlayerNetOwner: boolean, syncedMeta?: Partial<Entity["syncedMeta"]>) => void
+  syncedMetaChange?: (entity: T, syncedMeta: Partial<EntitySyncedMeta<T>>) => void
+  netOwnerChange?: (entity: T, isLocalPlayerNetOwner: boolean, syncedMeta?: EntitySyncedMeta<T>) => void
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any

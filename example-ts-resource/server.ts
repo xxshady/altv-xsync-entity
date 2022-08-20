4d550aa43c423ede686156a02e5eec35423e9268
xshady <fastik036@gmail.com>
2022-08-20 22:13:08 +0300

    update example resource

--- example-ts-resource/server.ts
+++ example-ts-resource/server.ts
@@ -1,47 +1,26 @@
 import * as alt from "alt-server"
 import type { IMarkerSyncedMeta } from "./shared"
 import { EntityPools } from "./shared"
-import { XSyncEntity, EntityPool, Entity } from "altv-xsync-entity-server"
+import * as xsync from "altv-xsync-entity-server"
 
-new XSyncEntity(
-  100,
-  // xsyncentity will create websocket ws:// (http) server
-  // that will use local machine ip for connecting on client (alt.WebSocketClient)
-  // for e.g. your external ip is 9.9.9.9
-  // remote client will use "ws://9.9.9.9:7700" in alt.WebSocketClient
-  {
+new xsync.XSyncEntity({
+  wss: {
     port: 7700,
     localhost: true,
   },
+})
 
-  // for wss:// (https) server you will need to specify domain and ssl certificate
-  // {
-  //   domainName: 'domain.name',
-  //   port: 7700,
-
-  //   // paths for fs.readFileSync
-  //   keyPath: './key.pem',
-  //   certPath: './cert.cert',
-
-  //   localhost: false
-  // },
-
-  // you can also specify useWss if you need http server and connect client to wss://domain.name
-  // for e.g. this will create http server on server and use connect url "wss://domain.name" on client in alt.WebSocketClient
-  // {
-  //   domainName: 'domain.name',
-  //   port: 7700,
-  //   useWss: false,
-  //   localhost: false,
-  // }
-)
-
-const markersPool = new EntityPool(
+const markersPool = new xsync.EntityPool(
   EntityPools.Marker,
   { maxStreamedIn: 50 },
 )
 
-class Marker extends Entity<IMarkerSyncedMeta> {
+@xsync.onEntityEvents<Marker>(markersPool, {
+  syncedMetaChange: (marker, changedData) => {
+    alt.log("Marker syncedMetaChange |", "id:", marker.id, "changed data:", changedData)
+  },
+})
+class Marker extends xsync.Entity<IMarkerSyncedMeta> {
   constructor(pos: alt.IVector3, type: number) {
     super(
       markersPool,
@@ -69,7 +48,7 @@ const marker = new Marker(
   2,
 )
 
-console.log('marker:', marker)
+alt.log("created marker id:", marker.id)
 
 // this will send updated partial synced meta to the client in "syncedMetaChange" event
 marker.setSyncedMeta({ type: 4 })
